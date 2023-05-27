46e5e667be79d581e1c65f0fd0def4a377031a31
Rakith Ranawarana <imrakith@gmail.com>
2023-05-27 18:23:55 +0530

    Fix the issue with chakkra menu

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { Grid, GridItem, HStack, Show } from "@chakra-ui/react";
+import { Box, Flex, Grid, GridItem, HStack, Show } from "@chakra-ui/react";
 import NavBar from "./components/NavBar";
 import GameGrid from "./components/GameGrid";
 import GenreList from "./components/GenreList";
@@ -42,19 +42,21 @@ function App() {
         </GridItem>
       </Show>
       <GridItem area="main">
-        <HStack spacing={5} paddingLeft={2} marginBottom={5}>
-          <PlatformSelector
-            selectedPlatform={gameQuery.platform}
-            onSelectedPlatform={(platform) =>
-              setGameQuery({ ...gameQuery, platform })
-            }
-          ></PlatformSelector>
+        <Flex paddingLeft={2} marginBottom={5}>
+          <Box marginRight={5}>
+            <PlatformSelector
+              selectedPlatform={gameQuery.platform}
+              onSelectedPlatform={(platform) =>
+                setGameQuery({ ...gameQuery, platform })
+              }
+            ></PlatformSelector>
+          </Box>  
           <SortSelector
             onSelectedOrder={(sortOrder) =>
               setGameQuery({ ...gameQuery, sortOrder })}
             selectedSortOrder={gameQuery.sortOrder}
           ></SortSelector>
-        </HStack>
+        </Flex>
 
         <GameGrid selectedGameQuery={gameQuery}></GameGrid>
       </GridItem>
